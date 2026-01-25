5555074aebedc7cdab40f18643841165148c6933
FilipMS <filipochamanda@gmail.com>
2026-01-25 17:47:40 +0100

    added red button kill

--- main.lua
+++ main.lua
@@ -12,3 +12,21 @@ end
 
 mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.EvaluateCache)
 
+local bigRedButton = Isaac.GetItemIdByName("Big Red Button")
+
+function mod:RedButtonUse()
+    local roomEntities = Isaac.GetRoomEntities()
+    for _, entity in ipairs(roomEntities) do
+        if entity:IsActiveEnemy() and entity:IsVulnerableEnemy() then
+            entity:Kill()
+        end
+    end
+
+    return {
+        Discharge = true,
+        Remove = false,
+        ShowAnim = true
+    }
+end
+
+mod:AddCallback(ModCallbacks.MC_USE_ITEM, mod.RedButtonUse, bigRedButton)
