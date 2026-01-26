e78c0bfbc536bac5c5a492dbcf4ee79fce96b1c2
FilipMS <filipochamanda@gmail.com>
2026-01-26 16:40:39 +0100

    added tutorial pollen

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 local mod = RegisterMod("MinPerfektaIsaacMod", 1)
+
 local damagePotion = Isaac.GetItemIdByName("Damage Potion")
 local damagePotionDamage = 1
 
@@ -11,6 +12,7 @@ function mod:EvaluateCache(player, cacheFlags)
 end
 
 mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.EvaluateCache)
+
 
 local bigRedButton = Isaac.GetItemIdByName("Big Red Button")
 
@@ -30,3 +32,39 @@ function mod:RedButtonUse()
 end
 
 mod:AddCallback(ModCallbacks.MC_USE_ITEM, mod.RedButtonUse, bigRedButton)
+
+
+local POLLEN_ITEM_ID = Isaac.GetItemIdByName("Pollen")
+local POLLEN_POISON_CHANCE = 0.4
+local POLLEN_POISON_LENGTH = 3
+local ONE_INTERVAL_OF_POISON = 20
+
+local game = Game()
+
+function mod:PollenNewRoom()
+    local playerCount = game:GetNumPlayers()
+
+    for playerIndex = 0, playerCount - 1 do
+        local player = Isaac.GetPlayer(playerIndex)
+        local copyCount = player:GetCollectibleNum(POLLEN_ITEM_ID)
+
+        if copyCount > 0 then
+            local rng = player:GetCollectibleRNG(POLLEN_ITEM_ID)
+
+            local entities = Isaac.GetRoomEntities()
+            for _, entity in ipairs(entities) do
+                if entity:IsActiveEnemy() and entity:IsVulnerableEnemy() then
+                    if rng:RandomFloat() < POLLEN_POISON_CHANCE then
+                        entity:AddPoison(
+                            EntityRef(player),
+                            POLLEN_POISON_LENGTH + (ONE_INTERVAL_OF_POISON * copyCount),
+                            player.Damage
+                        )
+                    end
+                end
+            end
+        end
+    end
+end
+
+mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.PollenNewRoom)
